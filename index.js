662e0520088ffca6f60a7fdc3bdbe601a451fd75
cathy-th <email2catherin.t@gmail.com>
2023-09-01 15:43:38 +0530

    mdcwbled

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    /*pagination: {
-      el: '.swiper-pagination',
-    },*/
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  },
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  autoplay: {
+      delay: 3000, // Set the delay between slides in milliseconds
+      disableOnInteraction: false, // Allow manual interaction (like clicking) to not stop the autoplay
+  },
+
+});
